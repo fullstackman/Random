6c35bf796507c2227d5f63c213c67bec859de884
Brandon Diaz <brandon.diaz.abreu@gmail.com>
2017-09-21 17:42:07 -0400

    Unit 1, Quiz

---
+++ Python/PMDS/Unit 1/quiz_1.py
@@ -0,0 +1,8 @@
+# The coding question of Quiz #1. He's cool with us just copying and pasting from our IDLE shell.
+print("\nWelcome to quiz number 1\n")
+givenNumber = int(input("Tell me your favorite number: "))
+if(givenNumber > 10 and givenNumber < 100):
+  print("This number is in range.")
+else:
+  print("Out of range.")
+print("Thank you for sharing. Bye bye!\n")
